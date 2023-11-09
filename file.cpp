c5860213247aea9398a3af451ce388361088d210
Mefiu207 <mefiu9278@gmail.com>
2023-11-09 19:40:50 +0100

    file

--- file.cpp
+++ file.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main (void)
 {
-    cout << "3x3" << endl;    
+    cout << "3x3" << endl;
+    
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "|";
+        }
+        cout << 
+    }
     return 0;
 }
